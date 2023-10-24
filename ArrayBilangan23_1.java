b238b75f5b0effcbb2f045ae7f11ff1154abacf7
nazwanrlwjy <144089599+nazwanrlwjy@users.noreply.github.com>
2023-10-24 13:56:17 +0700

    Add files via upload

---
+++ ArrayBilangan23_1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ArrayBilangan23_1{
+    public static void main (String[] args){
+
+        int [] bil = {5, 13, -7, 17} ;
+
+        for (int i : bil){
+            System.out.println(i);
+        }
+    }
+} 
