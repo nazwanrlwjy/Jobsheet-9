0a3fdf7a9918793af5475d4805d630ebe236eadc
nazwanrlwjy <144089599+nazwanrlwjy@users.noreply.github.com>
2023-10-24 14:50:31 +0700

    Add files via upload

---
+++ ArrayNilai23_2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+   
+public class ArrayNilai23_2 {
+  public static void main(String args[]){  
+    Scanner sc23 = new Scanner(System.in);
+
+     int [] nilaiAkhir = new int [10];
+
+    for (int i = 0; i < nilaiAkhir.length; i++) {
+        System.out.print("Masukkan nilai akhir ke-" + i + " : ");
+        nilaiAkhir [i] = sc23.nextInt();
+    }
+    for (int i = 0; i < nilaiAkhir.length; i++) {
+        if (nilaiAkhir[i] > 70){
+        System.out.println("Mahasiswa ke-" + i + " lulus! ");
+        }
+    }
+  }
+}
